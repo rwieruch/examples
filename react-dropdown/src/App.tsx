3d32a3d31d78aa2461b5166f9b99b9d6d81fcf29
Robin Wieruch <hello@rwieruch.com>
2024-10-07 14:50:15 +0200

    feat(react-dropdown): new

---
+++ react-dropdown/src/App.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+
+import "./App.css";
+
+const App = () => {
+  const handleMenuOne = () => {
+    console.log("clicked one");
+  };
+
+  const handleMenuTwo = () => {
+    console.log("clicked two");
+  };
+
+  return (
+    <Dropdown
+      trigger={<button>Dropdown</button>}
+      menu={[
+        <button onClick={handleMenuOne}>Menu 1</button>,
+        <button onClick={handleMenuTwo}>Menu 2</button>,
+      ]}
+    />
+  );
+};
+
+const Dropdown = ({ trigger, menu }) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className="dropdown">
+      {React.cloneElement(trigger, {
+        onClick: handleOpen,
+      })}
+      {open ? (
+        <ul className="menu">
+          {menu.map((menuItem, index) => (
+            <li key={index} className="menu-item">
+              {React.cloneElement(menuItem, {
+                onClick: () => {
+                  menuItem.props.onClick();
+                  setOpen(false);
+                },
+              })}
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+};
+
+export default App;
