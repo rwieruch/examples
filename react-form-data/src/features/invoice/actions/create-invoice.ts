de96c4cb77088cc2c6de80a9b58cba70b546ce3b
Robin Wieruch <hello@rwieruch.com>
2024-11-07 11:05:46 +0100

    fix(asd): asd

--- react-form-data/src/features/invoice/actions/create-invoice.ts
+++ react-form-data/src/features/invoice/actions/create-invoice.ts
@@ -1,14 +1,37 @@
-import { z } from "zod";
+// import { z } from "zod";
 
-const createInvoiceSchema = z.object({
-  title: z.string().min(3).max(191),
-  amount: z.coerce.number().positive(),
+// const createInvoiceSchema = z.object({
+//   title: z.string().min(3).max(191),
+//   amount: z.coerce.number().positive(),
+//   draft: z.coerce.boolean(),
+//   features: z.array(z.string()).optional(),
+//   opts: z.array(z.string()).optional(),
+// });
+
+// export const createInvoice = (formData: FormData) => {
+//   const { title, amount, draft, features, opts } = createInvoiceSchema.parse({
+//     ...Object.fromEntries(formData),
+//     features: formData.getAll("features"),
+//     opts: formData.getAll("opts"),
+//   });
+
+//   console.log(title, amount, draft, features, opts);
+// };
+
+import { z } from "zod";
+import { zfd } from "zod-form-data";
+
+const createInvoiceSchema = zfd.formData({
+  title: zfd.text(z.string().min(3).max(191)),
+  amount: zfd.numeric(z.number().positive()),
+  draft: zfd.checkbox(),
+  features: zfd.repeatable(),
+  opts: zfd.repeatable(),
 });
 
 export const createInvoice = (formData: FormData) => {
-  const { title, amount } = createInvoiceSchema.parse(
-    Object.fromEntries(formData)
-  );
+  const { title, amount, draft, features, opts } =
+    createInvoiceSchema.parse(formData);
 
-  console.log(title, amount);
+  console.log(title, amount, draft, features, opts);
 };
--- react-form-data/src/features/invoice/components/invoice-create-form.tsx
+++ react-form-data/src/features/invoice/components/invoice-create-form.tsx
@@ -11,6 +11,21 @@ const InvoiceCreateForm = () => {
       <label htmlFor="amount">Amount:</label>
       <input type="number" name="amount" id="amount" />
 
+      <label htmlFor="draft">Draft:</label>
+      <input type="checkbox" name="draft" id="draft" />
+
+      <label htmlFor="feature1">Feature 1:</label>
+      <input type="checkbox" name="features" value="feature1" id="feature1" />
+
+      <label htmlFor="feature2">Feature 2:</label>
+      <input type="checkbox" name="features" value="feature2" id="feature2" />
+
+      <label htmlFor="opts">Options:</label>
+      <select name="opts" multiple id="opts">
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+      </select>
+
       <button type="submit">Send</button>
     </form>
   );
