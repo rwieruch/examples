02a857c7fa51358e60aeb921394622f0ab7eaae2
Robin Wieruch <hello@rwieruch.com>
2025-01-06 13:47:39 +0100

    fix(asd): asd

--- react-router/src/App.tsx
+++ react-router/src/App.tsx
@@ -1,35 +1,153 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import {
+  Route,
+  Routes,
+  Outlet,
+  NavLink,
+  NavLinkRenderProps,
+  Link,
+  useParams,
+  useNavigate,
+  useSearchParams,
+} from "react-router";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Home = () => {
+  return (
+    <>
+      <h2>Home</h2>
+    </>
+  );
+};
+
+type User = {
+  id: string;
+  fullName: string;
+};
+
+type UsersProps = {
+  users: User[];
+};
+
+const Users = ({ users }: UsersProps) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const searchTerm = searchParams.get("name") || "";
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.value;
+
+    if (name) {
+      setSearchParams({ name: event.target.value });
+    } else {
+      setSearchParams({});
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <h2>Users</h2>
 
-export default App
+      <input type="text" value={searchTerm} onChange={handleSearch} />
+
+      <ul>
+        {users
+          .filter((user) =>
+            user.fullName.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+          )
+          .map((user) => (
+            <li key={user.id}>
+              <Link to={`/users/${user.id}`}>{user.fullName}</Link>
+            </li>
+          ))}
+      </ul>
+
+      <Outlet />
+    </>
+  );
+};
+
+type UserProps = {
+  onRemoveUser: (userId: string | undefined) => void;
+};
+
+const User = ({ onRemoveUser }: UserProps) => {
+  const { userId } = useParams();
+
+  return (
+    <>
+      <h2>User: {userId}</h2>
+
+      <button type="button" onClick={() => onRemoveUser(userId)}>
+        Remove
+      </button>
+
+      <Link to="/users">Back to Users</Link>
+    </>
+  );
+};
+
+const App = () => {
+  const navigate = useNavigate();
+
+  const [users, setUsers] = useState([
+    { id: "1", fullName: "Robin Wieruch" },
+    { id: "2", fullName: "Sarah Finnley" },
+  ]);
+
+  const handleRemoveUser = (userId: string | undefined) => {
+    setUsers((state) => state.filter((user) => user.id !== userId));
+
+    navigate("/users");
+  };
+
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="users" element={<Users users={users} />}>
+          <Route
+            path=":userId"
+            element={<User onRemoveUser={handleRemoveUser} />}
+          />
+        </Route>
+        <Route path="*" element={<NoMatch />} />
+      </Route>
+    </Routes>
+  );
+};
+
+const NoMatch = () => {
+  return <p>There's nothing here: 404!</p>;
+};
+
+const Layout = () => {
+  const style = ({ isActive }: NavLinkRenderProps) => ({
+    fontWeight: isActive ? "bold" : "normal",
+  });
+
+  return (
+    <>
+      <h1>React Router</h1>
+
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <NavLink to="/" style={style}>
+          Home
+        </NavLink>
+        <NavLink to="/users" style={style}>
+          Users
+        </NavLink>
+      </nav>
+
+      <main style={{ padding: "1rem 0" }}>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default App;
--- react-router/src/main.tsx
+++ react-router/src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
